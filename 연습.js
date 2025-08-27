ece1c7fe1b4b2049d50a0b6ba10fa028a8a287f2
namjinw <0812jinwoo@gmail.com>
2025-08-27 16:32:15 +0900

    Add files via upload

--- 연습.js
+++ 연습.js
@@ -1,11 +1,30 @@
-const h1 = document.querySelector(".h1");
+window.onload = function () {
+    const video = document.querySelector(".videos");
 
-let observe = new IntersectionObserver((e) => {
-    e.forEach(entry => {
-        if(entry.isIntersecting) {
-            entry.target.style.opacity = "1";
-            entry.target.style.transform = "translateY(0%)";
-        }
-    });
-});
-observe.observe(h1)
+    window.playVideo = function() {
+        video.play();
+    }
+    window.playpause = function() {
+        video.pause();
+    }
+    window.playstop = function() {
+        video.pause();
+        video.currentTime = 0;
+    }
+    window.playback = function() {
+        video.currentTime = Math.max(0, video.currentTime - 10);
+    }
+    window.playforward = function() {
+        video.currentTime = Math.min(video.duration, video.currentTime + 10);
+    }
+    window.playspeedup = function() {
+        video.playbackRate += 0.1;
+    }
+    window.playspeeddown = function() {
+        video.playbackRate = Math.max(0.1, video.playbackRate - 0.1);
+    }
+    window.playspeedreset = function() {
+        video.playbackRate = 1;
+    }
+} // <= 무조건 window.load(화면 로딩 전부 다 될때까지 기다리기)
+// <= window.함수명 (전역 함수 선언)
